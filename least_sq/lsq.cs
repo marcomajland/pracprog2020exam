2d8cb6c3210db2ed7aa2cdf90195d46edd7dca38
Marco Majland <marco@majland.net>
2020-03-24 12:26:19 +0100

    Updated least square routine.

--- least_sq/lsq.cs
+++ least_sq/lsq.cs
@@ -3,16 +3,16 @@ using static System.Console;
 using static System.Math;
 class lsq{
 	public static int Main(){
-		string[] xs = System.IO.File.ReadAllLines("x.txt");
-		string[] ys = System.IO.File.ReadAllLines("y.txt");
+		string[] xs = System.IO.File.ReadAllLines("x.txt"); // Raw x data
+		string[] ys = System.IO.File.ReadAllLines("y.txt"); // Raw y data
 		double[] x = new double[xs.Length];
 		double[] y = new double[xs.Length];
 		for (int i=0;i<x.Length;i++){
-			x[i] = Log(double.Parse(xs[i])); // Logarithmic value
+			x[i] = double.Parse(xs[i]);
 			y[i] = Log(double.Parse(ys[i])); // Logarithmic value
 		}
-		double[] dy = new double[y.Length];
-		for(int i=0;i<dy.Length;i++){dy[i]=(y[i]/20)/y[i];} // MUST BE FIXED
+		double[] dy = new double[y.Length]; // Allocate array for y errors
+		for(int i=0;i<dy.Length;i++){dy[i]= (Exp(y[i])/20)/Exp(y[i]);} // MUST BE FIXED
 		Func<double,double>[] F = new Func<double,double>[] {s => 1, s => s};
 		matrix A = new matrix(x.Length,F.Length);
 		for(int i=0;i<F.Length;i++){
@@ -24,23 +24,32 @@ class lsq{
 		for(int i=0;i<x.Length;i++){
 			b[i] = y[i]/dy[i];
 		}
-
 		qr AQR = new qr(A);
 		vector c = AQR.solve(b);
 		double a = Exp(c[0]);
-		double l = Exp(-c[1]);
-		double[] t = new double[100];
-		for(int i=0;i<100;i++){t[i] = i;}
-		var output = new System.IO.StreamWriter("output.txt",append:true);
-		for(int i=0;i<t.Length;i++){
-			output.WriteLine($"{t[i]} {exp(a,l)(t[i])}");
+		double l = c[1];
+		WriteLine($"a = {a}");
+		WriteLine($"lambda = {l}");
+		WriteLine($"t = {-Log(2)/l}");
+		var expout = new System.IO.StreamWriter("expout.txt",append:false);
+		var logout1 = new System.IO.StreamWriter("logout1.txt",append:false);
+		var logout2 = new System.IO.StreamWriter("logout2.txt",append:false);
+		for(int i=0;i<x.Length;i++){
+			expout.WriteLine($"{x[i]} {exp(a,l)(x[i])}");
 		}
-		output.Close();
-		
+		expout.Close();
+		for(int i=0;i<x.Length;i++){
+			logout1.WriteLine($"{x[i]} {y[i]}");
+		}
+		logout1.Close();
+		for(int i=0;i<x.Length;i++){
+			logout2.WriteLine($"{x[i]} {c[0] + c[1]*x[i]}");
+		}
+		logout2.Close();
 		return 0;
 	}
 	public static Func<double,double> exp(double a, double l){
-		Func<double,double> f = delegate(double x){return a*Exp(-l*x);};
+		Func<double,double> f = delegate(double x){return a*Exp(l*x);};
 		return f;
 	}
 }
