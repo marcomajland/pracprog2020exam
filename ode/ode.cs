03d922f8f8a07853ec058739d4ba80f5ac388bd4
Marco Majland <marco@majland.net>
2020-04-15 14:40:15 +0200

    Updated ODE routine and solved for cos(x) and SIR model.

--- ode/ode.cs
+++ ode/ode.cs
@@ -4,17 +4,42 @@ using static System.Math;
 using static System.Console;
 class ode{
 	public static int Main(){
-		double a = 0;
-		double b = 2*PI;
-		vector ya = new vector(1, 0);
-		Func<double,vector,vector> ODE = delegate(double x, vector y){		
-			return new vector(y[1], 1 - y[0]);
-		};			
-		matrix ys = ode_solver.rk12(ODE, ya, a, b);
+		// Test of ODE routine for y(x) = cos(x)
+		double a = 0; double b = 2*PI;
+		vector ya = new vector(2); ya[0] = 1; ya[1] = 0;
+		Func<double,vector,vector> cos_ODE = delegate(double x, vector y){		
+			return new vector(y[1], -y[0]);
+		};
+		Tuple<List<double>, List<vector>> cos_res = ode_solver.rk12(cos_ODE, ya, a, b);
 
-//		for(int i=0;i<n;i++){
-//			WriteLine($"{xs[i]} {ys[i][0]}");
-//		}
+
+
+		a = 0; b = 10;
+		double N = 100; double T_c = 1; double T_r = 1;
+		ya = new vector(3); ya[0] = 80; ya[1] = 10; ya[2] = 0;
+		Func<double,vector,vector> sir_ODE = delegate(double x, vector y){		
+			return new vector(-1/(N*T_c)*y[0]*y[1], 1/(N*T_c)*y[0]*y[1] - 1/T_r*y[1], 1/T_r*y[1]);
+		};
+		Tuple<List<double>, List<vector>> sir_res = ode_solver.rk12(sir_ODE, ya, a, b);
+
+		WriteLine("A: Jacobi diagonalization with cyclic sweeps and quantum particle in a box:");
+		WriteLine("------------------------------------------------");
+		WriteLine("Diagonalization of random real symmetric matrix:");
+		WriteLine("------------------------------------------------");
+
+		var cos_out = new System.IO.StreamWriter($"cos_out.txt",append:false);
+		for(int i=0;i<cos_res.Item1.Count;i++){
+			cos_out.WriteLine($"{cos_res.Item1[i]} {cos_res.Item2[i][0]} {Cos(cos_res.Item1[i])}");
+		}
+		cos_out.Close();
+		var sir_out = new System.IO.StreamWriter($"sir_out.txt",append:false);
+		for(int i=0;i<sir_res.Item1.Count;i++){
+			sir_out.WriteLine($"{sir_res.Item1[i]} {sir_res.Item2[i][0]} {sir_res.Item2[i][1]} {sir_res.Item2[i][2]}");
+		}
+		sir_out.Close();
+
+
+
 		return 0;
 	}
 }
--- ode/ode_solver.cs
+++ ode/ode_solver.cs
@@ -1,51 +1,36 @@
 using System;
+using static System.Console;
 using static System.Math;
+using System.Collections.Generic;
 public partial class ode_solver{
-	public static matrix rk12(Func<double,vector,vector> f, vector ya, double a, double b, 
-		double acc=1e-3, double eps=1e-3, double h=0.1, int n=999){
-		return driver(f, ya, a, b, h, acc, eps, n);
+	public static Tuple<List<double>, List<vector>> rk12(Func<double,vector,vector> f, vector ya, double a, double b, 
+		double acc=1e-1, double eps=1e-1, double h=0.01){return driver(f, ya, a, b, acc, eps, h);}
+	public static vector[] rkstep12(Func<double, vector, vector> f, double x, vector y, double h){
+		vector k_0 = f(x,y);
+		vector k_12 = f(x + 0.5*h, y + 0.5*h*k_0);
+		vector k = k_12;
+
+		vector yh = y + h*k;
+		vector err = (k - k_0)*h;
+	
+		return new vector[] {yh, err};
 	}
-
-	public static void rkstep12(
-		Func<double,vector,vector> f, double t, vector yt,
-		double h, vector yh, vector dy){
-			vector k_0 = new vector(yt.size);
-			vector k_12 = new vector(yt.size);
-			vector k_s = new vector(yt.size);
-
-			k_0 = f(t,yt);
-			k_12 = f(t + 1/2*h, yt + 1/2*h*k_0);
-			yh = yt + h*k_12;
-			k_s = f(t, yt);
-			dy = h*(k_12 - k_s);
-	}
-
-	public static matrix driver(Func<double, vector, vector> f, vector ya, double a, double b,
-		double h, double acc, double eps, int n){	
-			vector xs = new vector(n);
-			matrix ys = new matrix(ya.size,n);
-			vector yh = new vector(n);
-			vector dy = new vector(n);
-			double s; double normy=0; double tol; double x; vector y; double err=0;
-			xs[0] = a;
-			int i=0;
-			while(xs[i]<b){
-				x = xs[i]; y = ys[i];
-				if(x + h > b){h = b-x;}
-				rkstep12(f, x, y, h, yh, dy);
-				s=0;
-				for(int j=0;j<n;j++){s+=dy[j]*dy[j]; err = Sqrt(s);}
-				s=0;
-				for(int j=0;j<n;j++){s+=yh[j]*yh[j]; normy = Sqrt(s);}
-				tol = (normy*eps+acc)*Sqrt(h/(b-a));
-				if(err<tol){
-					i++;
-					if(i>n-1){i=-i;}
-					xs[i] = x+h; for(int j=0;j<n;j++){ys[i][j] = yh[j];}
-				}
-				if(err>0){h*=Pow(tol/err,0.25)*0.95;}else{h*=2;}
-			}	
-			return ys;
+	public static Tuple<List<double>, List<vector>> driver(Func<double,vector,vector> f, vector ya, double a, double b, 									double acc, double eps, double h){
+		List<double> xs = new List<double>();
+		List<vector> ys = new List<vector>();
+		double x; vector y; vector yh; double dyh; double tau;
+		xs.Add(a);
+		ys.Add(ya);
+		int i=0;
+		while(xs[i] < b-h){
+			x = xs[i]; y = ys[i];
+			vector[] step = rkstep12(f,x,y,h);
+			yh = step[0]; dyh = step[1].norm();
+			tau = (eps*yh.norm() + acc)*Sqrt(h/(b-a));
+			if(dyh < tau){i++; x+=h; xs.Add(x); ys.Add(yh);}
+			if(dyh > 0){h*=Pow(tau/dyh,0.25)*0.95;}else{h*=2;}
+		}
+		return new Tuple<List<double>, List<vector>>(xs, ys);
 	}
 }
 
