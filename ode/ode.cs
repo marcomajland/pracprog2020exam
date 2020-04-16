b2c0fafc920f9225f513e484339315ddf46305df
Marco Majland <marco@majland.net>
2020-04-16 17:19:07 +0200

    Updated ODE routine: added several values of T_c in SIR model.

--- ode/ode.cs
+++ ode/ode.cs
@@ -16,25 +16,32 @@ class ode{
 
 		// B: The SIR model
 		a = 0; b = 100; // Integration limits
-		double N = 5.8; double T_c = 3.0; double T_r = 14.0; // Model parameters
+		double N = 5.8; double T_c = 0; double T_r = 14.0; // Model parameters
 		ya = new vector(3); ya[0] = N-ya[1]; ya[1] = N*0.05; ya[2] = 0; // Initial value conditions for ODEs
 		// Construction of ODEs
-		Func<double,vector,vector> sir_ODE = delegate(double x, vector y){		
+		Func<double,vector,vector> sir_ODE = delegate(double x, vector y){
 			return new vector(-1/(N*T_c)*y[0]*y[1], 1/(N*T_c)*y[0]*y[1] - 1/T_r*y[1], 1/T_r*y[1]);
 		};
 		// ODE routine which returns x values and y values
-		Tuple<List<double>, List<vector>> sir_res = ode_solver.rk12(sir_ODE, ya, a, b);
+		Tuple<List<double>, List<vector>> sir_res;
+		for(T_c=0.25;T_c<2;T_c+=0.5){
+			sir_res = ode_solver.rk12(sir_ODE, ya, a, b);
+			var sir_out = new System.IO.StreamWriter($"sir_out_T_c_{T_c}.txt",append:false);
+			for(int i=0;i<sir_res.Item1.Count;i++){
+				sir_out.WriteLine($"{sir_res.Item1[i]} {sir_res.Item2[i][0]} {sir_res.Item2[i][1]} {sir_res.Item2[i][2]}");
+			}sir_out.Close();
+		}
+		
+
+
+
+
 		// Export results to separate text files for plotting using gnuplot
 		var cos_out = new System.IO.StreamWriter($"cos_out.txt",append:false);
 		for(int i=0;i<cos_res.Item1.Count;i++){
 			cos_out.WriteLine($"{cos_res.Item1[i]} {cos_res.Item2[i][0]} {Cos(cos_res.Item1[i])}");
 		}
 		cos_out.Close();
-		var sir_out = new System.IO.StreamWriter($"sir_out.txt",append:false);
-		for(int i=0;i<sir_res.Item1.Count;i++){
-			sir_out.WriteLine($"{sir_res.Item1[i]} {sir_res.Item2[i][0]} {sir_res.Item2[i][1]} {sir_res.Item2[i][2]}");
-		}
-		sir_out.Close();
 
 		// Text to be printed in console with no importance to numerical calculations
 		WriteLine("Ordinary differential equations");
