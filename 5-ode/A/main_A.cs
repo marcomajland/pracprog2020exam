be181b146b9a1bc2f7a6cee39d82326c67827808
Marco Majland <marco@majland.net>
2020-05-19 12:34:35 +0200

    Finished problem 5A but with unexpected periodic error in y'' = -y

--- 5-ode/A/main_A.cs
+++ 5-ode/A/main_A.cs
@@ -5,16 +5,17 @@ using static System.Console;
 class ode{
 	public static int Main(){
 		// ODE routine test for ODE u'' = -u
-		double a = 0; double b = 2*PI; // Integration limits
+		double a = 0; double b = 4*PI; // Integration limits
 		vector ya = new vector(2); ya[0] = 1; ya[1] = 0; // Initial value conditions for u(x)
 		Func<double,vector,vector> cos_ODE = delegate(double x, vector y){return new vector(y[1], -y[0]);};
 		// ODE routine which returns x values and u,u'
-		Tuple<List<double>, List<vector>> cos_res = ode_solver.rk12(cos_ODE, ya, a, b, 1e-3, 1e-3);
+		double acc = 1e-3; double eps = 1e-3; double h = 0.01; int max_steps = 999;
+		Tuple<List<double>, List<vector>> cos_res = ode_solver.rk12(cos_ODE, ya, a, b, acc, eps, h, max_steps);
 
 		// Export results to separate text files for plotting using gnuplot
 		var cos_out = new System.IO.StreamWriter($"./plot_files/cos_out.txt",append:false);
 		for(int i=0;i<cos_res.Item1.Count;i++){
-			cos_out.WriteLine($"{cos_res.Item1[i]} {cos_res.Item2[i][0]} {Cos(cos_res.Item1[i])}");
+			cos_out.WriteLine($"{cos_res.Item1[i]} {cos_res.Item2[i][0]} {Cos(cos_res.Item1[i])} {Cos(cos_res.Item1[i]) - cos_res.Item2[i][0]}");
 		}
 		cos_out.Close();
 		return 0;
--- matlib/ode_solver.cs
+++ matlib/ode_solver.cs
@@ -5,8 +5,8 @@ using System.Collections.Generic;
 public partial class ode_solver{
 	// Initializing method with default accuracy, epsilon and stepsize
 	public static Tuple<List<double>, List<vector>> rk12(Func<double,vector,vector> f, vector ya, double a, double b, 
-		double acc=1e-1, double eps=1e-1, double h=0.01){
-			return driver(f, ya, a, b, acc, eps, h);
+		double acc=1e-3, double eps=1e-3, double h=0.01, int step_limit = 999){
+			return driver(f, ya, a, b, acc, eps, h, step_limit);
 	}
 	// Runge-Kutta stepper utilizing the Euler midpoint method which returns updated y-values and estimated error
 	public static vector[] rkstep12(Func<double, vector, vector> f, double x, vector y, double h){
@@ -19,44 +19,42 @@ public partial class ode_solver{
 		return new vector[] {yh, err};
 	}
 	// Adaptive step size driver routine which utilizes the Runge-Kutta stepper with the Euler midpoint method
-	public static Tuple<List<double>, List<vector>> driver(Func<double,vector,vector> f, vector ya, double a, double b, 									double acc, double eps, double h){
+	public static Tuple<List<double>, List<vector>> driver(Func<double,vector,vector> f, vector ya, double a, double b, 									double acc, double eps, double h, int step_limit){
 		List<double> xs = new List<double>(); // List to contain x-values
 		List<vector> ys = new List<vector>(); // List to contain y-values
-		double x; vector y; vector yh; vector dyh; vector tau; int n=0; int step_limit = 999;
-		vector ratios;
+		double x; vector y; vector yh; vector dyh; vector tau; int n=0; vector ratios;
 		xs.Add(a); // Add initial x-value to list
 		ys.Add(ya); // Add initial y-value to list
 		do{
 			x = xs[n]; y = ys[n];
-			if(n>step_limit){return new Tuple<List<double>, List<vector>>(xs, ys);}
 			if(x>=b){return new Tuple<List<double>, List<vector>>(xs, ys);}
-			if(a+h > b){h = b-a;}
+			if(n>step_limit){return new Tuple<List<double>, List<vector>>(xs, ys);}
+			if(x+h > b){h = b-x;}
 			vector[] step = rkstep12(f,x,y,h);
 			yh = step[0]; dyh = step[1];
 			bool accept_step = true;
 			tau = new vector(dyh.size);
 			ratios = new vector(dyh.size);
 			for(int i=0;i<tau.size;i++){;
-				tau[i] = Max(acc,Abs(yh[i])*eps)*Sqrt(h/(b-a));
-				if(Abs(dyh[i]) > Abs(tau[i])) {accept_step = false;}
-				ratios[i] = Abs(tau[i])/Abs(dyh[i]);
+				tau[i] = (eps*Abs(yh[i]) + acc)*Sqrt(h/(b-a));
+				if(Abs(dyh[i]) > tau[i]){accept_step = false;}
+				ratios[i] = Abs(tau[i]/dyh[i]);
 			}
 			double fac = ratios[0];
 			for(int i=1;i<tau.size;i++){
 				fac = Min(fac,ratios[i]);
 			}
-			h *= Min(Pow(fac,0.25)*0.95,2);
-			for(int i=0;i<tau.size;i++){if(Abs(dyh[i]) > tau[i]){accept_step = false;}}
+			double h_fac = Pow(fac,0.25)*0.95;
+			if(h_fac < 2){h*=h_fac;}else{h*=2;}
 			if(accept_step){
+				n++;
 				x += h;
 				xs.Add(x);
 				ys.Add(yh);
-				n++;
 			}
 		}while(true);
 	}
 }
-
 
 
 
