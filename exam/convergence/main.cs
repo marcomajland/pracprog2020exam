01e38bb207df9355c6766a25ee9de49586ea528f
Marco Majland <marco@majland.net>
2020-06-20 10:30:29 +0200

    changed initial eigenvector guess in inverse iteration to random vector

--- exam/convergence/main.cs
+++ exam/convergence/main.cs
@@ -23,50 +23,11 @@ class main{
 		double[] deviations = new double[3] {1.01, 1.02, 1.03};
 		for(int j=0;j<deviations.Length;j++){
 			e_0 = e[i]*deviations[j];
-//			v_0 = V[i]/V[i].norm();
-//			for(int k=0;k<v_0.size;k++){v_0[k] = v_0[k]*deviations[j];}
 			matrix I = new matrix(A.size1,A.size1); I.set_identity();
-			generate_convergences(j, ref Ac, ref I, e_0, v_0, e[i], tol, n_max, updates);
+			power_method.generate_convergences(j, ref Ac, ref I, e_0, v_0, e[i], tol, n_max, updates);
 		}
 		return 0;
 	}	
-	public static void generate_convergences(int iteration, ref matrix A, ref matrix I, double e_0, vector v_0, double e_J, double tol = 1e-6, int n_max = 999, int updates = 999){
-		matrix As; double s = e_0;
-		As = A - s*I;
-		qr As_QR = new qr(As);
-		
-		List<double> errors = new List<double>();
-		//List<double> rayleigh_errors = new List<double>();
-		
-		generate_errors(As_QR, ref A, ref I, ref errors, updates, e_J, v_0, tol);
-
-		var outfile = new System.IO.StreamWriter($"./plotfiles/convergence_{iteration}.txt",append:false);
-		for(int k=0;k<errors.Count;k++){outfile.WriteLine($"{k} {errors[k]}");}
-		outfile.Close();	
-	}
-	public static void generate_errors(qr As_QR, ref matrix A, ref matrix I, ref List<double> errors, int updates, double e_J, vector v_0, double tol, double n_max = 999){
-		int n = 0; int m = 0;
-		matrix As;
-		double s; vector u; vector v;
-		u = v_0/v_0.norm();
-		double error = 1.0;
-		while(error > tol && n < n_max){
-			v = As_QR.solve(u);
-			v = v/v.norm();
-			error = (v - u).norm();
-			errors.Add(error);
-			if(error > 1.99){break;}
-			u = v/v.norm();
-			if(m > updates){
-				m = 0;
-				s = u.dot(A*u)/(u.dot(u));
-				As = A - s*I;
-				As_QR = new qr(As);
-			}
-			n++; m++;
-		}
-		s = u.dot(A*u)/(u.norm()*u.norm());
-	}
 }
 
 
--- exam/power_method.cs
+++ exam/power_method.cs
@@ -35,31 +35,22 @@ public partial class power_method{
 		s = u.dot(A*u)/(u.norm()*u.norm());
 		return new double[] {s, n, error};
 	}
-	public static List<double> generate_convergences(int iteration, matrix A, double e_0, vector v_0, double e_J, double tol = 1e-6, int n_max = 999, int max_qrs = 5){
-		matrix As; matrix I;
-		I = new matrix(A.size1,A.size1); I.set_identity();
-		double s = e_0;
+	public static void generate_convergences(int iteration, ref matrix A, ref matrix I, double e_0, vector v_0, double e_J, double tol = 1e-6, int n_max = 999, int updates = 999){
+		matrix As; double s = e_0;
 		As = A - s*I;
 		qr As_QR = new qr(As);
 		
 		List<double> errors = new List<double>();
-		List<double> rayleigh_errors = new List<double>();
+		//List<double> rayleigh_errors = new List<double>();
 		
-		generate_errors(A, As_QR, ref errors, 999, e_J, v_0, tol);
-		generate_errors(A, As_QR, ref rayleigh_errors, 2, e_J, v_0, tol);
+		generate_errors(As_QR, ref A, ref I, ref errors, updates, e_J, v_0, tol);
 
 		var outfile = new System.IO.StreamWriter($"./plotfiles/convergence_{iteration}.txt",append:false);
 		for(int k=0;k<errors.Count;k++){outfile.WriteLine($"{k} {errors[k]}");}
 		outfile.Close();	
-		var rayleigh_out = new System.IO.StreamWriter($"./plotfiles/rayleigh_convergence_{iteration}.txt",append:false);
-		for(int k=0;k<rayleigh_errors.Count;k++){rayleigh_out.WriteLine($"{k} {rayleigh_errors[k]}");}
-		rayleigh_out.Close();
-
-		return errors;
 	}
-	public static void generate_errors(matrix A, qr As_QR, ref List<double> errors, int m_max, double e_J, vector v_0, double tol, double n_max = 999){
+	public static void generate_errors(qr As_QR, ref matrix A, ref matrix I, ref List<double> errors, int updates, double e_J, vector v_0, double tol, double n_max = 999){
 		int n = 0; int m = 0;
-		matrix I = new matrix(A.size1,A.size1); I.set_identity();
 		matrix As;
 		double s; vector u; vector v;
 		u = v_0/v_0.norm();
@@ -68,21 +59,19 @@ public partial class power_method{
 			v = As_QR.solve(u);
 			v = v/v.norm();
 			error = (v - u).norm();
+			errors.Add(error);
+			if(error > 1.99){break;}
 			u = v/v.norm();
-			if(m > m_max){
+			if(m > updates){
 				m = 0;
 				s = u.dot(A*u)/(u.dot(u));
 				As = A - s*I;
 				As_QR = new qr(As);
 			}
-			n++; m++; errors.Add(error);
+			n++; m++;
 		}
 		s = u.dot(A*u)/(u.norm()*u.norm());
-		if(s - e_J > tol){WriteLine($"Error: Wrong eigenvalue");}
 	}
-
-
-
 }
 
 
