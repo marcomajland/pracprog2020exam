5379bfcf5afd1cd0633546be8cd36e7dedf9dd4e
Marco Majland <marco@majland.net>
2020-06-20 09:38:09 +0200

    added test output for exam problems

--- exam/main.cs
+++ exam/main.cs
@@ -3,10 +3,54 @@ using static System.Console;
 using System.Collections.Generic;
 using static System.Math;
 class main{
-	public static int Main(){
-		
+	public static int Main(){	
+	int dim = 30;
+		double tol = 1e-6;
+		int updates = 0;
+		int n_max = 999;
+		var rnd = new Random(); int i = rnd.Next(dim);
+		double deviation = 1.05;
 
-		convergence();
+		matrix A = misc.gen_matrix(dim);
+		matrix Ac = A.copy();
+
+		var jacobi = new jacobi_diagonalization(A);
+		vector e = jacobi.get_eigenvalues(); 
+		matrix V = jacobi.get_eigenvectors(); 
+
+		double e_0 = e[i]*deviation;
+		vector v_0 = V[i]/V[i].norm();
+		for(int j=0;j<v_0.size;j++){v_0[j] = v_0[j]*deviation;}
+
+		double[] s = power_method.inverse_iteration(Ac, e_0, v_0, tol, n_max, updates, false);
+
+
+		var outfile = new System.IO.StreamWriter($"test_out.txt",append:false);
+		outfile.WriteLine($"--------------------------------------------");
+		outfile.WriteLine($"Inverse iteration method (Jacobi comparison)");
+		outfile.WriteLine($"--------------------------------------------");
+		outfile.WriteLine($"To test the implementation of the inverse iteration method, the algorithm is compared to the Jacobi diagonalization procedure which we implemented in problems 4.");
+		outfile.WriteLine($"A random real symmetric matrix of dimensions {dim}x{dim} is generated and diagonalized using Jacobi diagonalization (implementation may be found in matlib). A random eigenvalue of the Jacobi procedure is chosen as initial guess for the inverse iteration. The initial guess is defined as e_0 = delta*e_i where delta is the deviation from the Jacobi eigenvalue, e_i. The Jacobi eigenvalues are ordered in increasing order such that e_{i-1}<e_{i}<e_{i+1}.\n");
+		outfile.WriteLine($"Random eigenvalue index:     {i}\n");
+		outfile.WriteLine($"Jacobi eigenvalues:");
+		outfile.WriteLine($"e_{i-1}:                         {e[i-1]}");
+		outfile.WriteLine($"e_{i}:                         {e[i]}");
+		outfile.WriteLine($"e_{i+1}:                         {e[i+1]}\n");
+		outfile.WriteLine($"Inverse iteration method:");
+		outfile.WriteLine($"Deviation:                    {deviation}");
+		outfile.WriteLine($"Initial eigenvalue:           {e_0}");
+		outfile.WriteLine($"Algorithm result:             {s[0]}");
+		outfile.WriteLine($"Error:                        {s[2]}");
+		outfile.WriteLine($"Iterations:                   {s[1]}");
+		outfile.WriteLine($"Rayleigh updates:             {updates}\n");
+		outfile.WriteLine($"Comparison to Jacobi diagonalization:");
+		outfile.WriteLine($"Abs(e_{i-1} - s):                {Abs(e[i-1]-s[0])}");
+		outfile.WriteLine($"Abs(e_{i} - s):                {Abs(e[i]-s[0])}");
+		outfile.WriteLine($"Abs(e_{i+1} - s):                {Abs(e[i+1]-s[0])}\n");
+		outfile.WriteLine($"Eigenvectors of Jacobi diagonalization:");
+		outfile.WriteLine("");
+		outfile.Close();	
+
 
 /*		int dim = 20; int iterations = 300;
 		double min_dev = 0.80; double max_dev = 1.20; double step_dev = 0.01;
@@ -14,7 +58,8 @@ class main{
 */
 		return 0;
 	}
-	public static double[] generate_s(double deviation, int dim, double tol = 1e-6, int max_qrs = 0, int n_max = 999){
+
+/*	public static double[] generate_s(double deviation, int dim, double tol = 1e-6, int max_qrs = 0, int n_max = 999){
 		var rnd = new Random(); int i = rnd.Next(dim);
 		matrix A = misc.gen_matrix(dim); matrix Ac = A.copy();
 
@@ -26,7 +71,7 @@ class main{
 		vector v_0 = V[i]/V[i].norm();
 		for(int j=0;j<v_0.size;j++){v_0[j] = v_0[j]*deviation;}
 
-		double[] s = power_method.inverse_iteration(Ac, e_0, v_0, tol, n_max, max_qrs);
+		double s = power_method.inverse_iteration(Ac, e_0, v_0, tol, n_max, max_qrs);
 		return new double[] {s[0], e[i]};		
 	}
 	public static int sample_convergences(int dim, double deviation, int iterations, double tol = 1e-3){
@@ -112,6 +157,7 @@ class main{
 		WriteLine($"Inverse iteration method eigenvalue:               {s}");
 		WriteLine($"Error (dev. from Jacobi):                          {Abs(s-e[i])}");
 */
+
 
 
 
--- exam/power_method.cs
+++ exam/power_method.cs
@@ -2,8 +2,9 @@ using System;
 using static System.Console;
 using static System.Math;
 using static System.Double;
+using System.Collections.Generic;
 public partial class power_method{
-	public static double[] inverse_iteration(matrix A, double e_0, vector v_0, double tol = 1e-6, int n_max = 999, int max_qrs = 5){
+	public static double[] inverse_iteration(matrix A, double e_0, vector v_0, double tol = 1e-6, int n_max = 999, int updates = 5, bool error_msg = false){
 		int n = 0; int m = 0; double error = 1.0;
 		double s; vector u; vector v;
 		matrix As; matrix I;
@@ -13,11 +14,12 @@ public partial class power_method{
 		As = A - s*I;
 		qr As_QR = new qr(As);
 		while(error > tol && n < n_max){
+//			if(error == 2){break;}
 			v = As_QR.solve(u);
 			v = v/v.norm();
 			error = (v - u).norm();
 			u = v/v.norm();
-			if(m > max_qrs){
+			if(m > updates){
 				m = 0;
 				s = u.dot(A*u)/(u.dot(u));
 				As = A - s*I;
@@ -25,10 +27,63 @@ public partial class power_method{
 			}
 			n++; m++;
 		}
+		if(n == n_max && error_msg){WriteLine($"Warning: maximum iterations reached, maybe wrong eigenvector");}
+		else if(error_msg){WriteLine($"Iteration results:");
+			WriteLine($"Iterations:          {n}");
+			WriteLine($"Error:               {error}");
+			WriteLine($"Rayleigh:            {updates}");
+		}
 		s = u.dot(A*u)/(u.norm()*u.norm());
-//		WriteLine($"Inverse iteration iterations:    {n}");
-		return new double[2] {s,n};
+		return new double[] {s, n, error};
 	}
+	public static List<double> generate_convergences(int iteration, matrix A, double e_0, vector v_0, double e_J, double tol = 1e-6, int n_max = 999, int max_qrs = 5){
+		matrix As; matrix I;
+		I = new matrix(A.size1,A.size1); I.set_identity();
+		double s = e_0;
+		As = A - s*I;
+		qr As_QR = new qr(As);
+		
+		List<double> errors = new List<double>();
+		List<double> rayleigh_errors = new List<double>();
+		
+		generate_errors(A, As_QR, ref errors, 999, e_J, v_0, tol);
+		generate_errors(A, As_QR, ref rayleigh_errors, 2, e_J, v_0, tol);
+
+		var outfile = new System.IO.StreamWriter($"./plotfiles/convergence_{iteration}.txt",append:false);
+		for(int k=0;k<errors.Count;k++){outfile.WriteLine($"{k} {errors[k]}");}
+		outfile.Close();	
+		var rayleigh_out = new System.IO.StreamWriter($"./plotfiles/rayleigh_convergence_{iteration}.txt",append:false);
+		for(int k=0;k<rayleigh_errors.Count;k++){rayleigh_out.WriteLine($"{k} {rayleigh_errors[k]}");}
+		rayleigh_out.Close();
+
+		return errors;
+	}
+	public static void generate_errors(matrix A, qr As_QR, ref List<double> errors, int m_max, double e_J, vector v_0, double tol, double n_max = 999){
+		int n = 0; int m = 0;
+		matrix I = new matrix(A.size1,A.size1); I.set_identity();
+		matrix As;
+		double s; vector u; vector v;
+		u = v_0/v_0.norm();
+		double error = 1.0;
+		while(error > tol && n < n_max){
+			v = As_QR.solve(u);
+			v = v/v.norm();
+			error = (v - u).norm();
+			u = v/v.norm();
+			if(m > m_max){
+				m = 0;
+				s = u.dot(A*u)/(u.dot(u));
+				As = A - s*I;
+				As_QR = new qr(As);
+			}
+			n++; m++; errors.Add(error);
+		}
+		s = u.dot(A*u)/(u.norm()*u.norm());
+		if(s - e_J > tol){WriteLine($"Error: Wrong eigenvalue");}
+	}
+
+
+
 }
 
 
