a7769a8247aedd7f701d5bafe03d59db8d90b4b8
Marco Majland <marco@majland.net>
2020-06-21 22:25:10 +0200

    added fit to problem 4B

--- numerical/4-eigen/B/main_B.cs
+++ numerical/4-eigen/B/main_B.cs
@@ -10,15 +10,31 @@ class eigen{
 		int nmax = 100;
 		Stopwatch time = new Stopwatch();
 		var diag_scale = new System.IO.StreamWriter("./plot_files/diag_scale.txt",append:false);
-		for(int n=2;n<nmax;n+=1){
+		for(int n=20;n<nmax;n+=1){
 			A = misc.gen_matrix(n);
 			time.Reset();
 			time.Start();
 			var res = new jacobi_diagonalization(A);
 			time.Stop();
-			diag_scale.WriteLine($"{n} {time.ElapsedMilliseconds}");
+			diag_scale.WriteLine($"{Log(n)} {Log(time.ElapsedMilliseconds)}");
 		}
 		diag_scale.Close();
+
+		// Linear fit to logarithmic data to verify n^3 dependence
+		List<double[]> data = misc.load_data("./plot_files/diag_scale.txt");
+		double[] x = data[0]; double[] y = data[1];
+		double[] dy = new double[y.Length]; // Allocate array for y errors
+		for(int i=0;i<dy.Length;i++){dy[i]= 1e-3;} // Logarithmic y errors
+		Func<double,double>[] F = new Func<double,double>[] {s => 1, s => s}; // Array of fitting functions
+		var fit = new lsq_qr(x,y,dy,F); // Variable storing instance of least square class
+		vector c = fit.get_c(); // Retrieve expansion coefficients of fitting functions
+		double a = c[0];
+		double b = c[1];
+		var fit_data = new System.IO.StreamWriter("./plot_files/fit_data.txt",append:false);
+		for(double n=x[0];n<x[x.Length-1];n+=0.1){
+			fit_data.WriteLine($"{n} {a + b*n}");
+		}
+		fit_data.Close();
 
 		// 2) Demonstration of value-by-value method
 		int dim = 10; int eigenvalues = 5;
@@ -27,6 +43,13 @@ class eigen{
 		var A_res = new jacobi_diagonalization(A,"cyclic");
 		var Ac_res = new jacobi_diagonalization(Ac,"value",eigenvalues);
 		var outfile = new System.IO.StreamWriter("../out_B.txt",append:false);
+		outfile.WriteLine($"---------------------------------");
+		outfile.WriteLine($"Scaling of matrix diagonalization");
+		outfile.WriteLine($"---------------------------------");
+		outfile.WriteLine($"To demonstrate the O(n^3) scaling of the matrix diagonalization procedure, a linear fit is applied to the logaritmic (n,t) data.");
+		outfile.WriteLine($"Fit result:");
+		outfile.WriteLine($"log(t) = {a} + {b}*log(n)");
+		outfile.WriteLine($"Thus, the scaling must be O(n^3) since the slope is approximately equal to 3.\n");
 		outfile.WriteLine($"-----------------------------------------------");
 		outfile.WriteLine($"Jacobi diagonalization eigenvalue-by-eigenvalue");
 		outfile.WriteLine($"-----------------------------------------------");
